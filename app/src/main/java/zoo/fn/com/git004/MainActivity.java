d07f2d99f8cdba529bbbdb861a91bee2002810fe
Alex4952 <keyzoo@gmail.com>
2015-07-01 14:15:10 +0900

    3 include .iml and vcs.xml

--- app/src/main/java/zoo/fn/com/git004/MainActivity.java
+++ app/src/main/java/zoo/fn/com/git004/MainActivity.java
@@ -1,12 +1,12 @@
 package zoo.fn.com.git004;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 // 주석포함해서 commit
+// 두번째 주석 넣고
 public class MainActivity extends AppCompatActivity {
 
 	@Override
