200a4f9bc75e446310b0fb3cb3691549dcafdd86
Alex4952 <keyzoo@gmail.com>
2015-07-01 14:00:11 +0900

    2 from AlexVM

--- app/src/main/java/zoo/fn/com/git004/MainActivity.java
+++ app/src/main/java/zoo/fn/com/git004/MainActivity.java
@@ -2,11 +2,12 @@ package zoo.fn.com.git004;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+// 주석포함해서 commit
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
